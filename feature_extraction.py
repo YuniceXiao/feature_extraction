421a1c765566b354a023839f1ac5f913efd093e7
YuniceXiao <fishxy2@outlook.com>
2020-01-20 17:41:22 -0500

    assignment 1

--- feature_extraction.py
+++ feature_extraction.py
@@ -30,12 +30,16 @@ def main(data_file, vocab_path):
     reviews, ids = load_reviews(data_file)
 
     # TODO: appropriately shape and fill this matrix
+    # define the shape of this 2d array
     nrows = len(ids)
     ncols = len(function_words)
+    # initialize the 2d array
     review_features = np.zeros((nrows, ncols), dtype=np.int)
+    # fill in the value of the 2d array
     for i in range(len(reviews)):
+        #tokenize and lowercase all the words
         each_review = word_tokenize(reviews[i].lower())
-        # each_review = reviews[i].split()
+        #loop through each word of each review and fill in the value of the 2d array
         for word in each_review:
             if word in function_words:
                 word_index = function_words.index(word)
@@ -46,18 +50,21 @@ def main(data_file, vocab_path):
     print(f"Numpy array has shape {review_features.shape} and dtype {review_features.dtype}")
 
     # TODO: Calculate these from review_features
+    # sum up each column
     words_count = [sum(x) for x in zip(*review_features)]
+    # get the most common words
     most_common_count = max(words_count)
     most_common_word_index = words_count.index(most_common_count)
     most_common_word = function_words[most_common_word_index]
     print(f"Most common word: {most_common_word}, count: {most_common_count}")
 
     # TODO: Find any features that weren't in the data (i.e. columns that sum to 0)
+    # initialize a list for index whose column sum is zero
     zero_inds = []
+    # loop through the list of the sum of columns, append index whose column sum is zero to the list just initialized
     for i in range(len(words_count)):
         if words_count[i] == 0:
             zero_inds.append(i)
-
     if len(zero_inds) > 0:
         print("No instances found for: ")
         for ind in zero_inds:
@@ -69,7 +76,9 @@ def main(data_file, vocab_path):
     print(f"Sum of raw count matrix: {matrix_sum}")
 
     # TODO: make a binary feature vector from your count vector
+    # copy the 2d array and convert it to a binary vector
     word_binary = np.copy(review_features)
+    # loop through each entry and convert the value whose value is not zero to one
     for i in range(len(word_binary)):
         for j in range(len(word_binary[i])):
             if word_binary[i][j] > 0:
@@ -78,24 +87,32 @@ def main(data_file, vocab_path):
     print(f"Sum of binary matrix: {word_binary_sum}")
 
     # TODO: normalize features by review length (divide rows by number of words in the review)
+    # copy the matrix
     norm_reviews = np.copy(review_features)
+    # copy the numpy ndarray to a list
     norm_reviews = norm_reviews.tolist()
+    # loop through each row and calculate the sum of each row
     for i in range(len(norm_reviews)):
         sum_of_row = sum(norm_reviews[i])
+        # loop through each entry of each row and normalize it by the sum of each row
         for j in range(len(norm_reviews[i])):
             normalized_val = (norm_reviews[i][j]) / (sum_of_row)
             norm_reviews[i][j] = normalized_val
+    # convert the list back to a numpy array
     norm_reviews = np.array(norm_reviews)
+    #round the decimals
     norm_reviews_sum = round(norm_reviews.sum(), 2)
     print(f"Sum of normed matrix: {norm_reviews_sum}")
 
     # TODO: remove features from <review_features> that occur less than <min_count> times
     min_count = 100
     min_matrix = np.copy(review_features)
+    # initialize a list for index whose column sum is less than minimum ocunt
     remove_column_index = []
     for i in range(len(words_count)):
         if words_count[i] <= min_count:
             remove_column_index.append(i)
+    # remove columns whose column sum is less than the minimum count by np.delete(array, list of index to remove, axis = 1)
     min_matrix = np.delete(min_matrix, remove_column_index, 1)
     min_matrix_shape = min_matrix.shape
     print(f"Shape after removing features that occur < {min_count} times: {min_matrix_shape}")
